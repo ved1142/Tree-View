2ceef4d0d1e9af39981722f040f990074cc43e79
John Doe <vedu9030e@gmail.com>
2024-12-10 12:32:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Accordian from './components/Accordian';
 import RandomColor from './components/Accordian/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      <RandomColor/>
+
+      {/* <RandomColor/> */}
+      <StarRating/>
 
 
     </div>
