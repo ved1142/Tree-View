78b27e05ef6204f2babb5adb41dbce7e973e747a
Ved <vedu9030@gmail.com>
2024-12-10 18:34:30 +0530

    Initial commit for Image-slider project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Accordian from './components/Accordian';
 import RandomColor from './components/Accordian/random-color';
+import ImageSlider from './components/image-slider';
 import StarRating from './components/star-rating';
 
 function App() {
@@ -8,7 +9,13 @@ function App() {
     <div className="App">
 
       {/* <RandomColor/> */}
-      <StarRating/>
+      {/* <StarRating/> */}
+       <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
+
 
 
     </div>
